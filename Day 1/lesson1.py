e5e5e1ee805e8ef18f3ab10ef4db15434133310d
maxataaaa <anastasiamaxatadze53@gmail.com>
2024-01-30 14:23:40 +0400

    'Goa'

---
+++ Day 1/lesson1.py
@@ -0,0 +1,22 @@
+name='taso'
+
+
+surname = "maxatadze"
+
+print(name)
+
+name = "taso"
+age = 15
+height = 170.5 
+
+
+knows_programming=True
+is_ugly = False
+print(name+" "+surname)
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+
+print(name+' '+str(age))
+print('I am '+name+' '+(surname)+' My age is '+str(age )+' and I am '+str(height)+'cm')  
