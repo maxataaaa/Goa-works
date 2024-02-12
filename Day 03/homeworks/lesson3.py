4dae8a55a8fad96adca0b345f3eedb5793e56643
maxataaaa <anastasiamaxatadze53@gmail.com>
2024-02-12 14:01:00 +0400

    'goa'

--- Day 03/homeworks/lesson3.py
+++ Day 03/homeworks/lesson3.py
@@ -1,4 +1,11 @@
-print='koba'
-print='sophio'
-print='luka'
-print='gabriel'
+father = 39
+print(father + 2)
+
+mother = 36
+print(mother + 2)
+
+brother = 13 
+print(brother + 2)
+
+brother = 6 
+print(brother + 2)
