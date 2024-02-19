143798715187fc19410799c17f74bc66696522be
maxataaaa <anastasiamaxatadze53@gmail.com>
2024-02-19 19:51:09 +0400

    'goa'

---
+++ Day 05/classwork/lesson.py
@@ -0,0 +1,7 @@
+
+
+num1 = int(input("Please eneter first number: "))
+num2 = int(input("Please eneter second number: "))
+num3 = int(input("Please eneter third number: "))
+
+print(num1 * num2 * num3)
---
+++ Day 05/homework/lesson5.py
@@ -0,0 +1,27 @@
+num1 =int(input())
+num2 =int(input())
+print(num1==num2)
+
+num1 =int(input())
+num2 =int(input())
+print(num1>num2)
+
+num1 =int(input())
+num2 =int(input())
+print(num1<num2)
+
+num1 =int(input())
+num2 =int(input())
+print(num1 + num2)
+
+num1 =int(input())
+num2 =int(input())
+print(num1 - num2)
+
+num1 =int(input())
+num2 =int(input())
+print(num1 * num2)
+ 
+num1 =int(input())
+num2 =int(input())
+print(num1 / num2)
