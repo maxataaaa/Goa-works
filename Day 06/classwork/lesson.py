be8bbca131c5fc413441150ae04b1f2c6bb9a8ec
maxataaaa <anastasiamaxatadze53@gmail.com>
2024-02-24 14:42:24 +0400

    'goa'

---
+++ Day 06/classwork/lesson.py
@@ -0,0 +1,8 @@
+name =input('what is your name:')
+lastname = input('what is your last name:')
+age = int(input('what is your age:'))
+
+print(f'Hello my name is {name} {lastname} and i am {age} years old')
+
+
+
