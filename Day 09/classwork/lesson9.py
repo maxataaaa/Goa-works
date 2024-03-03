d502ff4a5af64c0c208640598d04cf504f71bb1b
maxataaaa <anastasiamaxatadze53@gmail.com>
2024-03-04 00:04:50 +0400

    'goa'

---
+++ Day 09/classwork/lesson9.py
@@ -0,0 +1,2 @@
+num = int(input('enter a number:'))
+print(num > 1 and num < 10)
---
+++ Day 09/homework/lesson9.py
@@ -0,0 +1,43 @@
+#num = int(input('enter number: '))
+#print(num % 2 == 0 and num % 4 == 0)
+
+
+#num1 = int(input('please enter your num1: '))
+#num2 = int(input('please enter your num2: '))
+#print(num1 == num2)
+#print(num1 >= num2)
+
+# num1 = int(input('please enter your num1: '))
+# num2 = int(input('please enter your num2: '))
+# print(num1 > num2)
+# print(num1 >= num2)
+
+# num1 = int(input('please enter your num1: '))
+# num2 = int(input('please enter your num2: '))
+# print(num1 < num2)
+# print(num1 <= num2 )
+
+# num1 = int(input('please enter your num1: '))
+# num2 = int(input('please enter your num2: '))
+# print(num1 == num2)
+# print(num1 >= num2)
+
+# num1 = int(input('please enter your num1: '))
+# num2 = int(input('please enter your num2: '))
+# print(num1 != num2)
+# print(num1 != num2)
+
+# num = int(input('enter num :'))
+# print(num < 7 and num > 8)
+# print(num > 5 and num > 6)
+# print(num > 2 and num < 10)
+# print(num < 12 and num > 4)
+# print(num > 1 and num > 7)
+# print(num < 10 and num < 14)
+# print(num > 3 and num > 8)
+# print(num > 8 and num < 11)
+# print(num > 3 and num < 8)
+# print(num > 6 and num < 6)
+
+
+
