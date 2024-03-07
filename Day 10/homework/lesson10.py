2e2f18c39859c658f3b8f95d787af1cc1af2d7c8
maxataaaa <anastasiamaxatadze53@gmail.com>
2024-03-08 00:31:34 +0400

    'goa'

--- Day 10/homework/lesson10.py
+++ Day 10/homework/lesson10.py
@@ -2,17 +2,27 @@ programminganguages_list = ['Javascript', ' Python','Go','Ruby','Perl']
 print(programminganguages_list)
 print(programminganguages_list[4])
 
-list = ['string','integer','boolean','float']
-print(list[0])
-print(list[1])
-print(list[2])
-print(list[3])
+list = ['anastasia',6,'false','6.7']
+print(list[0],list[1],list[2],list[3])
 
-number_list = [4,8,12,16,20]
-print('უდიდესი რივხვია',number_list[4])
+number_list = []
+for i in range(0,20):
+  if i % 4 == 0:
+    number_list.append(i)
+print(number_list )
 
 
-number_list = [31,33,35,37,39,41,43,45,47,49,51]
-number_list1 = number_list[0] + number_list[1] + number_list[2]
+number_list = []
+for i in range(30, 50 + 1):
+  if i % 2 == 1:
+    number_list.append(i)
+
+number_list = []
+for i in range(30, 50 + 1):
+  if i % 2 == 1:
+    number_list.append(i)
+  elif i % 3 == 0:
+    print(i)
 print(number_list)
-print(number_list1)
+print(number_list.append[0] + number_list.append[1] + number_list.append[2])
+
