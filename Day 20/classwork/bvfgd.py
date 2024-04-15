0c0105d52bd6ec213a626d304a04c3091b36ff91
maxataaaa <anastasiamaxatadze53@gmail.com>
2024-04-15 17:32:47 +0400

    'goa'

---
+++ Day 20/classwork/bvfgd.py
@@ -0,0 +1,6 @@
+my_list = [5,78,90,50,4,2,3,7,94,6,40]
+new_list = []
+for num in my_list:
+    if num % 2 == 0:
+        new_list.append(num)
+print(new_list)
---
+++ Day 20/classwork/ვბგფბრ.py
@@ -0,0 +1,6 @@
+num_list = [7,6,14,18,19,34,56,20]
+for i in range(0,len(num_list)):
+    if num_list[i] % 2 != 0:
+     print(num_list[i])
+
+
